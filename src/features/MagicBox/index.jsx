effe169c897aeebca1177ea3ca78255ef07f0272
Sonnguyen <sonnguyen.pk1301@gmail.com>
2020-11-18 21:37:55 +0700

    learn props and state

--- src/features/MagicBox/index.jsx
+++ src/features/MagicBox/index.jsx
@@ -1,32 +1,42 @@
-import Box from './components/Box';
+import { useState } from 'react';
 import BoxList from './components/BoxList';
 
+const FAKE_DATA = [
+  {
+    color: 'goldenrod',
+    luckyNumber: 2,
+    size: 'small',
+  },
+  {
+    color: 'deeppink',
+    luckyNumber: 5,
+    size: 'medium',
+  },
+  {
+    color: 'red',
+    luckyNumber: 10,
+    size: 'large',
+  },
+];
+
 function MagicBoxFeature() {
-  const box = { color: 'blue', luckyNumber: 13 };
-  const boxList = [
-    {
-      color: 'goldenrod',
-      luckyNumber: 2,
-    },
-    {
-      color: 'deeppink',
-      luckyNumber: 5,
-    },
-    {
-      color: 'red',
-      luckyNumber: 10,
-    },
-  ];
+  const [boxList, setBoxList] = useState(FAKE_DATA);
+
+  const handleBoxClick = (box, idx) => {
+    // remove box from list
+    console.log(box, idx);
+    const newBoxList = [...boxList];
+    newBoxList.splice(idx, 1);
+
+    setBoxList(newBoxList);
+    console.log(boxList);
+  }; // truyền hàm xuông con khi cần con sẽ gọi hàm
 
   return (
     <div>
       <h1>MagicBox Feature</h1>
 
-      <BoxList boxList={boxList} />
-
-      <Box color="green" luckyNumber={11} />
-      <Box color={box.color} luckyNumber={box.luckyNumber} />
-      <Box luckyNumber={4} />
+      <BoxList boxList={boxList} onBoxClick={handleBoxClick} />
     </div>
   );
 }
